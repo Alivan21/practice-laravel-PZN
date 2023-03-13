e6cc5de7d101a19d00a44cbc3b645a778e489219
alfanalivan <alfanalivan@gmail.com>
2023-03-13 09:12:07 +0700

    configuration and cache config

---
+++ config/contoh.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  "author" => [
+    "first" => env('NAME_FIRST', "Alivan"),
+    "last" => "Alfan"
+  ],
+  "email" => "alfanalivan@gmail.com",
+  "web" => "https://Alivan.my.id"
+];
